7b8aaa0d643b9695c1d81814fb123c7ce6248e59
TaielC <43725160+TaielC@users.noreply.github.com>
2019-06-20 18:26:16 -0300

    Punto_2 código

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from euler import *
 from runge_kutta import *
 from graficadora import *
 
+
 def transformar_a_celcius_minutos(resultados):
 	resultados_transformados = []
 	for i in range(len(resultados)):
 		resultados_transformados.append((resultados[i][0]/60, resultados[i][1] - 273.15))
 	return resultados_transformados
+
 
 def calcular_resultados_reales(funcion, h, punto_inicio, punto_final):
 	i = punto_inicio
@@ -17,11 +19,13 @@ def calcular_resultados_reales(funcion, h, punto_inicio, punto_final):
 		i+=h
 	return resultados
 
+
 def calcular_error_relativo(resultados_reales, resultados):
 	lista = []
 	for i in range(len(resultados_reales)):
 		lista.append((resultados_reales[i][0], abs(resultados_reales[i][1]-resultados[i][1])/resultados_reales[i][1]))
 	return lista
+
 
 def soaking(resultados):
 	tf = resultados[-1][0]
@@ -35,8 +39,8 @@ def soaking(resultados):
 				ti = resultado[0]
 			n+=1
 			temp+=resultado[1]
+	return (tf-ti,temp/n)
 
-	return (tf-ti),(temp/n)
 
 def Punto_1():
 	punto_inicio = 0 # s
@@ -55,3 +59,19 @@ def Punto_1():
 
 	guardar_grafico_error_relativo("Error Relativo Euler", error_relativo_euler)
 	guardar_grafico_error_relativo("Error Relativo Runge-Kutta", error_relativo_runge_kutta)
+
+
+def Punto_2():
+	punto_inicio = 0 # s
+	punto_final = longitud / velocidad # s
+	resolucion_runge_kutta = runge_kutta(funcion_completa, cadencia, punto_inicio, punto_final, T_0)
+
+	guardar_grafico_tiempo_vs_temperatura("Resultados obtenidos", resolucion_runge_kutta)
+
+	resultados_reales_punto_1 = calcular_resultados_reales(solucion_sin_radiacion, cadencia, punto_inicio, punto_final)
+	guardar_grafico_tiempo_vs_temperatura("Comparación de Resultados con Solución Analítica Sin Radiación", \
+		resultados_reales_punto_1, resolucion_runge_kutta)
+
+	tiempo_soaking, T_soaking = soaking(resolucion_runge_kutta)
+	print(f"Tiempo de Soaking: {tiempo_soaking/60}\n Temperatura Soaking: {T_soaking-273}")
+
